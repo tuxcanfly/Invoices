756095d9a40dc874d5b06fd068970e6241afd4c0
tuxcanfly <tuxcanfly@domU-12-31-39-09-42-12.compute-1.internal>
2010-06-06 12:44:10 +0000

    config changes

--- usware/settings.py
+++ usware/settings.py
@@ -12,7 +12,7 @@ MANAGERS = ADMINS
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3', # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': '/home/tuxcanfly/Work/usware/bills.db', # Or path to database file if using sqlite3.
+        'NAME': '/home/tuxcanfly/Work/github/usware/bills.db', # Or path to database file if using sqlite3.
         'USER': '',                      # Not used with sqlite3.
         'PASSWORD': '',                  # Not used with sqlite3.
         'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
@@ -81,7 +81,7 @@ TEMPLATE_DIRS = (
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
-	'/home/tuxcanfly/Work/usware/html'
+	'/home/tuxcanfly/Work/github/usware/html'
 )
 
 INSTALLED_APPS = (
